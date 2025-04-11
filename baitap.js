66b54121756524cf3b0046d655c957189385574d
chinhlocker <chinhminh2501@gmail.com>
2025-04-11 20:58:22 +0700

    Baitap

---
+++ baitap.js
@@ -0,0 +1,25 @@
+// var ten = prompt("Nhập tên")
+// var tuoi = prompt("Nhập tuổi")
+// var diaChiTruong = prompt("Nhập địa chỉ trường")
+// var soBan = prompt("Nhập số người bạn của bạn")
+
+// alert(`Tên tôi là ${ten}. Tôi ${tuoi}. Trường tôi ở ${diaChiTruong}. Tôi có ${soBan} người bạn.`)
+
+// var a = prompt("Nhập số a")
+// alert(a*2)
+
+// var a = prompt("Nhập số a")
+// var b = prompt("Nhập số b")
+// var c = prompt("Nhập số c")
+// alert((a-b)*c)
+
+// var a = Number(prompt("nhập số a"))
+// var b = Number(prompt("Nhập số b"))
+// alert(a+b+a*b)
+
+var a = Number(prompt("Nhập số a"))
+var b = Number(prompt("Nhập số b"))
+var c = Number(prompt("Nhập số c"))
+var p = (a+b+c)/2
+var d = Math.sqrt(p*(p-a)*(p-b)*(p-c))
+alert(d)
