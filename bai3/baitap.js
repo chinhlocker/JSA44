2b235a41e311e47bca8a69817ead78d18b519f20
chinhlocker <chinhminh2501@gmail.com>
2025-04-18 13:52:21 +0700

    baivn3

---
+++ bai3/baitap.js
@@ -0,0 +1,71 @@
+// var ten = prompt("Nhập tên")
+// var tuoi = prompt("Nhập tuổi")
+// var diaChiTruong = prompt("Nhập địa chỉ trường")
+// var soBan = prompt("Nhập số người bạn của bạn")
+
+// alert(`Tên tôi là ${ten}. Tôi ${tuoi}. Trường tôi ở ${diaChiTruong}. Tôi có ${soBan} người bạn.`)
+
+// bai1
+// var a = prompt("Nhập số a");
+// alert(a*2);
+
+// bai2
+// var a = prompt("Nhập số a");
+// var b = prompt("Nhập số b");
+// var c = prompt("Nhập số c");
+// alert((a-b)*c);
+
+// bai11
+// var a = Number(prompt("Nhập số a"));
+// var b = Number(prompt("Nhập số b"));
+// var c = Number(prompt("Nhập số c"));
+// var p = (a+b+c)/2;
+// var d = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+// alert(d);
+
+// bai3
+// var a = prompt("Nhập 1 chữ in hoa bất kì");
+// a = alert(a.toLowerCase());
+
+//bai4
+// var a = prompt("Nhập số nguyên dương a");
+// const b = (a-1)/2;
+// let c = 0;
+// for (let i = 1; i <= a; i++) {
+//     if (i > b) {
+//         c++;
+//     }
+// }
+// alert(c);
+
+//bai5 chưa làm đc
+// var a = prompt("nhập số a");
+// var b = 0;
+// for (let i = 1; i <= (a*a); i++) {
+//     if ((a+1) <= i) {
+//         b++;
+//     }
+// }
+// alert(b)
+
+//bai6
+// var a = prompt("nhập dãy chữ bị đảo ngược");
+// var b = "";
+// for (let i in a) {
+//     b = a[i] + b;
+// }
+// alert(b)
+
+// bai7
+// var a = Number(prompt("nhập số a"));
+// var b = Number(prompt("Nhập số b"));
+// alert(a+b+a*b);
+
+//bai8
+// var a = prompt("Nhập 1 chữ thường bất kì");
+// var b = a.toUpperCase();
+// alert(b);
+
+//bai9 ko biết làm
+
+//bai10 ko biết làm
---
+++ bai3/day3.js
@@ -0,0 +1,22 @@
+// console.log() => in  giá trị ra bảng console
+console.log("abc");
+
+// Kiểu dữ liệu trong javascript: chuỗi(string), số(number), true/flase(boolean), underfined(), null(), object()
+// Cách để kiểu dữ liệu: typeof
+// Chuỗi + số => 1 chuỗi mới
+// Biến là đại diện cho 1 trường dữ liệu n đó và giúp ng đọc dễ hiểu hơn
+// Khai báo biến trong js : let, var, const
+// Công thức: let/var/const tên_biến = gtri n đó
+// update gtri của biến
+var address = "Ha Noi"
+console.log('tôi đg ở' + address)
+
+// camal case: viết hoa chữ cái đầu của từ thứ 2 trở đi
+// snake case: các từ đc ngăn cách bởi dấu gạch dưới
+// ${tenBien}: dùng để đọc biến trong chuỗi
+
+// Hàm prompt trong js: cho phép ng dùng khai báo gtri từ trình duyệt
+// Hàm alert("Lời dẫn") => Thông báo ra ngoài trình duyệt
+
+// Phép tính cơ bản trong js: +, -, *, /, **
+// Dấu căn bậc 2: Math.sqrt()
